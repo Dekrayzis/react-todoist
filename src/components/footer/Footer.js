ad2dcbbe622427b1ac6060b18077310895f00b3d
Dekrayzis <vincent.corleone@live.com>
2020-12-01 18:14:45 +0000

    Initial setup

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./footer.scss";
+
+const Footer = () => {
+  return <footer className="app__footer">Footer</footer>;
+};
+
+export default Footer;
---
+++ src/components/header/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./header.scss";
+
+const Header = () => {
+  return <header className="app__header">Header</header>;
+};
+
+export default Header;
