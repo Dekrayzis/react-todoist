cf38854a3911db7e74ff145fc0c6c45eaa93ed7f
Dekrayzis <vincent.corleone@live.com>
2020-12-01 18:57:25 +0000

    Created content and sidebar components

---
+++ src/components/layout/Content.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+
+//-- Stylesheet
+import "./content.scss";
+
+const Content = () => {
+  return (
+    <div className="content">
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Content;
---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from "react-icons/fa";
+
+//-- Stylesheet
+import "./sidebar.scss";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar" data-testid="sidebar">
+      <ul className="sidebar__generic">
+        <li
+          data-testid="inbox"
+          className={active === "inbox" ? "active" : null}
+        >
+          <div
+            data-testid="inbox-action"
+            aria-label="Show inbox tasks"
+            tabIndex={0}
+            role="button"
+          >
+            <span>
+              <FaInbox />
+            </span>
+            <span>Inbox</span>
+          </div>
+        </li>
+        <li
+          data-testid="today"
+          className={active === "today" ? "active" : null}
+        >
+          <div
+            data-testid="today-action"
+            aria-label="Show today's tasks"
+            tabIndex={0}
+            role="button"
+          >
+            <span>
+              <FaRegCalendar />
+            </span>
+            <span>Today</span>
+          </div>
+        </li>
+        <li
+          data-testid="next_7"
+          className={active === "next_7" ? "active" : undefined}
+        >
+          <div
+            data-testid="next_7-action"
+            aria-label="Show tasks for the next 7 days"
+            tabIndex={0}
+            role="button"
+          >
+            <span>
+              <FaRegCalendarAlt />
+            </span>
+            <span>Next 7 days</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
